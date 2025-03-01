6b1e091766b8f387f78ff8da25660d57165e9e6f
Minh <dinhthanhminhk11.git@gmail.com>
2025-03-01 10:46:47 +0700

    clear code

--- app/src/main/java/code/name/monkey/retromusic/network/LastFMService.kt
+++ app/src/main/java/code/name/monkey/retromusic/network/LastFMService.kt
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) 2019 Hemanth Savarala.
- *
- * Licensed under the GNU General Public License v3
- *
- * This is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by
- *  the Free Software Foundation either version 3 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- */
-
 package code.name.monkey.retromusic.network
 
 import code.name.monkey.retromusic.network.model.LastFmAlbum
@@ -19,10 +5,6 @@ import code.name.monkey.retromusic.network.model.LastFmArtist
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
-
-/**
- * Created by hemanths on 2019-11-26.
- */
 
 interface LastFMService {
     companion object {
